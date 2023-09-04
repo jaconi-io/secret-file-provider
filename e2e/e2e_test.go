7a99e0e2852a1f70fa6d99db4ac7fa39cb31582f
Martin Hartmann <mhartmann@jaconi.io>
2023-09-04 13:56:31 +0200

    test: add e2d test for logging

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -3,12 +3,14 @@ package e2e_test
 import (
 	"bytes"
 	"context"
+	"io"
 	"testing"
 
 	"github.com/jaconi-io/secret-file-provider/e2e/testdata"
 	. "github.com/onsi/gomega"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
@@ -32,7 +34,44 @@ func TestExample(t *testing.T) {
 			g.Expect(stderr.String()).To(Equal(""))
 
 			return ctx
+		}).
+		Assess("logs are written", func(ctx context.Context, t *testing.T, c *envconf.Config) context.Context {
+			pods := &corev1.PodList{}
+			err := c.Client().Resources(c.Namespace()).List(ctx, pods)
+			g.Expect(err).NotTo(HaveOccurred())
+
+			pod := pods.Items[0]
+
+			logs, err := readLogs(ctx, c, pod)
+			g.Expect(err).NotTo(HaveOccurred())
+
+			g.Expect(logs).To(ContainSubstring("Successfuly written /secrets/secrets.yaml"))
+			g.Expect(logs).To(ContainSubstring("name=example"))
+			g.Expect(logs).To(ContainSubstring("namespace=" + pod.Namespace))
+
+			return ctx
 		}).Feature()
 
 	_ = testenv.Test(t, feat)
+}
+
+// readLogs gets the log output (stdout + stderr) from 'secret-file-provider' in a given pod.
+func readLogs(ctx context.Context, c *envconf.Config, pod corev1.Pod) (string, error) {
+
+	podLogOpts := &corev1.PodLogOptions{Container: "secret-file-provider"}
+	clientset, err := kubernetes.NewForConfig(c.Client().RESTConfig())
+	req := clientset.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, podLogOpts)
+
+	podLogs, err := req.Stream(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer podLogs.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = io.Copy(buf, podLogs)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
 }
